5d7487fc57829b4fca85077649578b32685ac4a1
RodolfoxvData <136007122+Rodolfoxxv@users.noreply.github.com>
2024-05-22 02:17:21 -0300

    yml

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "supabase_url" {
+  type = string
+  default = ""
+}
+
+variable "supabase_key" {
+  type = string
+  default = ""
+}
+
+variable "admin_password" {
+  type = string
+  default = ""
+}
